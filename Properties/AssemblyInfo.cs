0e0bb2f8cde0f50b5a8061f62243d2736593aedf
nader-dab <nader.dabour@abv.bg>
2016-05-26 13:44:44 +0300

    Upgraded to Sitefinity 9.1.6100.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("9.0.6000.0")]
-[assembly: AssemblyFileVersion("9.0.6000.0")]
+[assembly: AssemblyVersion("9.1.6100.0")]
+[assembly: AssemblyFileVersion("9.1.6100.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
