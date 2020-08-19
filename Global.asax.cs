f1d4c68b4f730fe938e30f75ec775ab26c79c34b
Georgi Georgiev <gg3orgiev@gmail.com>
2020-08-19 18:15:24 +0300

    Update Sampe to Sitefinity v.13.1.7400.0

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web;
+using System.Web.Helpers;
 using Telerik.Microsoft.Practices.Unity;
 using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing;
@@ -10,5 +11,14 @@ namespace SitefinityWebApp
 {
     public class Global : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("X-Frame-Options", "SAMEORIGIN");
+        }
+
+        protected void Application_Start()
+        {
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
+        }
     }
 }
