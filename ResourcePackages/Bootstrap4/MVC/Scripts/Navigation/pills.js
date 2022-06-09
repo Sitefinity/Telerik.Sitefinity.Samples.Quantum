5fdd0497e168ef8b822ecc9406c611f2af9a975d
Nader Dabour <dabour@progress.com>
2022-06-09 14:53:35 +0300

    Upgraded to SF 14.2.7900

---
+++ ResourcePackages/Bootstrap4/MVC/Scripts/Navigation/pills.js
@@ -0,0 +1,43 @@
+﻿(function () {
+/* Polyfills */
+
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    var initialized = false;
+    var initialize = function () {
+        if (!initialized) {
+            initialized = true;
+            document.querySelectorAll('.expandable-node').forEach(function (x) {
+                x.addEventListener('click', function (e) {
+                    if (window.DataIntelligenceSubmitScript && e.currentTarget.hasAttribute('aria-expanded') === 'false') {
+                        DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
+                            predicate: "Toggle navigation",
+                            object: e.currentTarget.innerText.trim(),
+                            objectMetadata: [{
+                                'K': 'PageTitle',
+                                'V': document.title
+                            },
+                            {
+                                'K': 'PageUrl',
+                                'V': location.href
+                            }
+                            ]
+                        });
+                    }
+                });
+            });
+        }
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+        initialize();
+    });
+
+    if (window.personalizationManager) {
+        window.personalizationManager.addPersonalizedContentLoaded(function () {
+            initialize();
+        });
+    }
+}());
